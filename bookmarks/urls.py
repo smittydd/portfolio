b8101ae65c2da081d706ca21109eba294ce16f7e
smittydd <flippinrealestate101@gmail.com>
2025-05-20 14:58:36 -0500

    Add files via upload
    This is an example django website I developed.

---
+++ bookmarks/urls.py
@@ -0,0 +1,15 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
+    path('__debug__/', include('debug_toolbar.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
