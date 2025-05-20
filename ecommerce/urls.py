88315dd893092b02a0b06a466afecd784970b75a
smittydd <flippinrealestate101@gmail.com>
2025-05-20 15:04:19 -0500

    Add files via upload
    This is an example of an e-commerce site I am developing.

---
+++ ecommerce/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    # Admin url
+    path('admin/', admin.site.urls),
+
+    # Store app
+    path('', include('store.urls') ),
+
+    # Cart app
+    path('cart/', include('cart.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
